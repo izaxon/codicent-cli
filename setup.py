42f952d72f9aa3f1de970b91732c7126248ba677
Johan Isaksson <johan@izaxon.com>
2025-05-22 10:13:59 +0200

    Add codicent-py as a dependency in requirements.txt and update setup.py to reflect changes

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 from setuptools import setup, find_packages
 
-with open("requirements.txt") as f:
-    required = f.read().splitlines()
+# with open("requirements.txt") as f:
+#     required = f.read().splitlines()
 
 setup(
     name='codicent',
     version='0.4.2',
     py_modules=['app'],
-    install_requires=required,
+    install_requires=[
+        'rich',
+        # Do NOT put git+ URLs here!
+    ],
     entry_points='''
         [console_scripts]
         codicent=app:main
